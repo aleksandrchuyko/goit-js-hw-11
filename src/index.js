e6cb1ef3878c323ba6631f3b92147826bfb1ddd4
aleksandrchuyko <chao.mess@gmail.com>
2022-05-24 23:14:39 +0300

    Pagination with buttun

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const refs = {
     gallery: document.querySelector('.gallery'),
 }
 
-let params = {
+const params = {
     key: '27605426-0fda2c4b0cfa268906a19ec96',
     q: '',
     image_type: 'photo',
@@ -25,14 +25,20 @@ let params = {
     per_page: 40
 };
 
+let totalHits = 0;
+
 function clearGallery() {
     refs.gallery.innerHTML = "";
+    params.page = 1;
+    totalHits = 0;
+    refs.loadMoreBtn.classList.add('is-hidden');
+    
 }
 
 function renderGallery(photosArr) {
     const cards = photosArr.reduce((acc, card) => {
         return acc += `<div class="photo-card">
-        <img src="${card.webformatURL}" alt="${card.tags}" loading="lazy" />
+        <img src="" alt="${card.tags}" loading="lazy" />
         <div class="info">
             <p class="info-item">
             <b>Likes</b>${card.likes}
@@ -51,17 +57,28 @@ function renderGallery(photosArr) {
     }, "");
 
     refs.gallery.insertAdjacentHTML("beforeend", cards);
+    if (params.page * params.per_page < totalHits) {
+        refs.loadMoreBtn.classList.remove('is-hidden');
+    } else {
+        refs.loadMoreBtn.classList.add('is-hidden');
+    }
+    
+    if ((params.page * params.per_page >= totalHits) && (params.page > 1)) {
+        Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`);
+    }
 }
+
+clearGallery();
 
 refs.searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     clearGallery();
-    params.page = 1;
     const searchQuery = e.currentTarget.elements["searchQuery"].value.trim();
     params.q = searchQuery;
     fetchPhotos(params).then(response => {
-        if (response.data.hits.length) {
-            Notiflix.Notify.info(`Hooray! We found ${response.data.totalHits} images.`);
+        totalHits = response.data.totalHits;
+        if (totalHits) {
+            Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
             renderGallery(response.data.hits);
         }
         else {
--- src/js/fetchPhotos.js
+++ src/js/fetchPhotos.js
@@ -1,12 +1,9 @@
 const axios = require('axios').default;
-const options = {
-    key: '27605426-0fda2c4b0cfa268906a19ec96'
-}
+
 const url = "https://pixabay.com/api/";
 export const fetchPhotos = async function (params) {
   try {
-    console.log({ params });
-      const response = await axios.get(url, {
+        const response = await axios.get(url, {
         params
       });
     return response;
